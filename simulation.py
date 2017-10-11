bc504624b6833604fdc80cb8ffbad76e2417326e
Marcin Mrugas <mrugacz95@gmail.com>
2017-10-11 23:43:38 +0200

    Add Dijkstra path finding

--- simulation.py
+++ simulation.py
@@ -1,24 +1,43 @@
 import random
 import sys
 from collections import deque
+from enum import Enum
+from heapq import heappop, heappush
 
+import numpy as np
 import pygame
+from pygame import gfxdraw
 from pygame.locals import *
 
-WINDOW_SIZE = (800, 480)
+WINDOW_SIZE = (100, 100)
 BACKGROUND_COLOR = (0, 0, 0)
-SEGMENT_SIZE = 10
+BLOCK_SIZE = 10
+BLOCKS_H, BLOCKS_V = WINDOW_SIZE[0] // BLOCK_SIZE, WINDOW_SIZE[1] // BLOCK_SIZE
 food: tuple = None
+
+
+class Direction(Enum):
+    NONE = 0
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+
+
+visited = np.zeros((BLOCKS_H, BLOCKS_V), dtype=bool)
+previous = np.zeros((BLOCKS_H, BLOCKS_V), dtype=np.dtype('int,int'))
 
 
 class Segment(object):
     _pos = None
 
-    def __init__(self, pos: tuple):
-        self.x, self.y = pos
-
-    def __init__(self, x: int, y: int):
-        self.x, self.y = x, y
+    def __init__(self, x: int = None, y: int = None, pos: tuple = None):
+        if pos:
+            self.x, self.y = pos
+        elif x and y:
+            self.x, self.y = x, y
+        else:
+            raise RuntimeError("Wrong segment initialization")
 
     @property
     def pos(self) -> tuple:
@@ -33,12 +52,20 @@ class Segment(object):
 
 snake = deque()
 
-direction = K_LEFT
+movement = K_LEFT
+
+
+def mark_snake_on_board():
+    for segment in snake:
+        visited[segment.x, segment.y] = True
+
+
+def random_pos():
+    return random.randint(0, BLOCKS_H - 1), random.randint(0, BLOCKS_V - 1)
 
 
 def init_snake():
-    x, y = (random.randint(0, WINDOW_SIZE[0] // SEGMENT_SIZE),
-            random.randint(0, WINDOW_SIZE[1] // SEGMENT_SIZE))
+    x, y = 5, 6
     snake.append(Segment(x, y))
     snake.append(Segment(x + 1, y))
     snake.append(Segment(x + 2, y))
@@ -46,7 +73,7 @@ def init_snake():
 
 def draw_segment(screen: pygame.Surface, segment: Segment):
     x, y = segment.pos
-    pygame.draw.rect(screen, (50, 180, 50), (x * SEGMENT_SIZE, y * SEGMENT_SIZE, SEGMENT_SIZE, SEGMENT_SIZE))
+    pygame.draw.rect(screen, (50, 180, 50), (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
 
 
 def draw_snake(screen: pygame.Surface):
@@ -56,8 +83,8 @@ def draw_snake(screen: pygame.Surface):
 
 def draw_food(screen: pygame.Surface):
     x, y = food
-    x, y = x * SEGMENT_SIZE + SEGMENT_SIZE // 2, y * SEGMENT_SIZE + SEGMENT_SIZE // 2
-    pygame.draw.circle(screen, (200, 50, 60), (x, y), SEGMENT_SIZE // 2)
+    x, y = x * BLOCK_SIZE + BLOCK_SIZE // 2, y * BLOCK_SIZE + BLOCK_SIZE // 2
+    pygame.gfxdraw.filled_circle(screen, x, y, BLOCK_SIZE // 2, (200, 50, 60))
 
 
 def draw_scene(screen):
@@ -68,24 +95,27 @@ def draw_scene(screen):
 
 
 def move_snake():
-    global direction
     tail = snake.pop()
     x = {
         K_LEFT: -1,
         K_RIGHT: 1,
-    }.get(direction, 0)
+        Direction.LEFT: -1,
+        Direction.RIGHT: 1,
+    }.get(movement, 0)
     y = {
         K_UP: -1,
         K_DOWN: 1,
-    }.get(direction, 0)
+        Direction.UP: -1,
+        Direction.DOWN: 1,
+    }.get(movement, 0)
     tail.pos = (snake[0].x + x, snake[0].y + y)
     if tail.x < 0:
-        tail.x = WINDOW_SIZE[0] // SEGMENT_SIZE
-    elif tail.x > WINDOW_SIZE[0] // SEGMENT_SIZE:
+        tail.x = BLOCKS_H - 1
+    elif tail.x >= BLOCKS_H:
         tail.x = 0
     elif tail.y < 0:
-        tail.y = WINDOW_SIZE[1] // SEGMENT_SIZE
-    elif tail.y > WINDOW_SIZE[1] // SEGMENT_SIZE:
+        tail.y = BLOCKS_V - 1
+    elif tail.y >= BLOCKS_V:
         tail.y = 0
     snake.appendleft(tail)
 
@@ -95,11 +125,68 @@ def init_food():
     food = (3, 4)
 
 
+def neighbours(point: tuple) -> tuple:
+    directions = [([0, -1], K_UP), ([0, 1], K_DOWN), ([-1, 0], K_LEFT), ([1, 0], K_RIGHT)]
+    point = np.asarray(point)
+    for vec, dir in directions:
+        direction = point + vec
+        if direction[0] >= BLOCKS_H:
+            direction[0] = 0
+        elif direction[0] < 0:
+            direction[0] = BLOCKS_H - 1
+        elif direction[1] >= BLOCKS_V:
+            direction[1] = 0
+        elif direction[1] < 0:
+            direction[1] = BLOCKS_V - 1
+        yield tuple(direction), dir
+
+
+def find_path():
+    global distance, previous, visited
+    visited.fill(False)
+    mark_snake_on_board()
+    head = snake[0]
+    visited[head.pos] = False
+    previous[head.pos] = (-1, -1)
+    heap = [(0, head.pos)]
+    closest_pos = head.pos
+    while closest_pos != food and len(heap) != 0:
+        closest_dist, closest_pos = heappop(heap)
+        for neighbour, direction in neighbours(closest_pos):
+            if not visited[neighbour]:
+                heappush(heap, (closest_dist + 1, neighbour))
+                previous[neighbour] = closest_pos
+        visited[closest_pos] = True
+    node = food
+    while tuple(previous[node]) != head.pos:
+        node = tuple(previous[node])
+    for neighbour, direction in neighbours(node):
+        if neighbour == head.pos:
+            return {K_UP: K_DOWN,
+                    K_DOWN: K_UP,
+                    K_LEFT: K_RIGHT,
+                    K_RIGHT: K_LEFT}[direction]
+    return Direction.NONE
+
+
+def check_food():
+    global food
+    if food == snake[0].pos:
+        snake.appendleft(Segment(pos=food))
+        global visited
+        visited.fill(False)
+        mark_snake_on_board()
+        x, y = np.where(visited == False)
+        i = np.random.randint(len(x))
+        random_position = (x[i], y[i])
+        food = random_position
+
+
 def main():
     init_snake()
     init_food()
     pygame.init()
-    screen = pygame.display.set_mode((800, 480))
+    screen = pygame.display.set_mode(WINDOW_SIZE)
 
     def input(events):
         for event in events:
@@ -108,16 +195,19 @@ def main():
             elif event.type == KEYDOWN:
                 allowed = {K_UP, K_DOWN, K_LEFT, K_RIGHT}
                 if event.key in allowed:
-                    global direction
-                    direction = event.key
+                    global movement
+                    movement = event.key
                 elif event.key == K_ESCAPE:
                     sys.exit(0)
 
     while True:
         input(pygame.event.get())
+        global movement
+        movement = find_path()
         move_snake()
+        check_food()
         draw_scene(screen)
-        pygame.time.delay(100)
+        pygame.time.delay(400)
 
 
 if __name__ == '__main__':
