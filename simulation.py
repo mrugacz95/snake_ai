e2d2edabddd0c56a6c4e2c4d962b10dc1f8ea9ea
Marcin Mrugas <mrugacz95@gmail.com>
2017-10-12 02:05:58 +0200

    Changed Dijkstra to A*, add some randomness

--- simulation.py
+++ simulation.py
@@ -3,17 +3,21 @@ import sys
 from collections import deque
 from enum import Enum
 from heapq import heappop, heappush
+from time import sleep
 
 import numpy as np
 import pygame
+from numba import jit
 from pygame import gfxdraw
 from pygame.locals import *
 
-WINDOW_SIZE = (100, 100)
+WINDOW_SIZE = (150, 150)
 BACKGROUND_COLOR = (0, 0, 0)
 BLOCK_SIZE = 10
 BLOCKS_H, BLOCKS_V = WINDOW_SIZE[0] // BLOCK_SIZE, WINDOW_SIZE[1] // BLOCK_SIZE
 food: tuple = None
+snake = deque()
+movement = K_LEFT
 
 
 class Direction(Enum):
@@ -29,7 +33,7 @@ previous = np.zeros((BLOCKS_H, BLOCKS_V), dtype=np.dtype('int,int'))
 
 
 class Segment(object):
-    _pos = None
+    _pos = (-1, -1)
 
     def __init__(self, x: int = None, y: int = None, pos: tuple = None):
         if pos:
@@ -50,11 +54,6 @@ class Segment(object):
         self.y = y
 
 
-snake = deque()
-
-movement = K_LEFT
-
-
 def mark_snake_on_board():
     for segment in snake:
         visited[segment.x, segment.y] = True
@@ -65,10 +64,13 @@ def random_pos():
 
 
 def init_snake():
-    x, y = 5, 6
+    x, y = random_pos()
+    x -= 2
     snake.append(Segment(x, y))
     snake.append(Segment(x + 1, y))
     snake.append(Segment(x + 2, y))
+    global food
+    food = (x, y)
 
 
 def draw_segment(screen: pygame.Surface, segment: Segment):
@@ -124,9 +126,10 @@ def init_food():
     global food
     food = (3, 4)
 
-
+@jit
 def neighbours(point: tuple) -> tuple:
     directions = [([0, -1], K_UP), ([0, 1], K_DOWN), ([-1, 0], K_LEFT), ([1, 0], K_RIGHT)]
+    np.random.shuffle(directions)
     point = np.asarray(point)
     for vec, dir in directions:
         direction = point + vec
@@ -141,8 +144,34 @@ def neighbours(point: tuple) -> tuple:
         yield tuple(direction), dir
 
 
-def find_path():
-    global distance, previous, visited
+@jit
+def points_2d_dist_torus(a: tuple, b: tuple):
+    mn = min(a[0], b[0])
+    mx = max(a[0], b[0])
+    xdist = min((mx - mn) ** 2, (mn + BLOCKS_H - mx) ** 2)
+
+    mn = min(a[1], b[1])
+    mx = max(a[1], b[1])
+    ydist = min((mx - mn) ** 2, (mn + BLOCKS_V - mx) ** 2)
+    return xdist + ydist
+
+
+def points_2d_dist_euclidean(a: tuple, b: tuple):
+    return np.linalg.norm(np.asarray(a) - np.asarray(b))  # We can pass edge so geometry is torus not plane
+
+
+def points_2d_dist_manhattan(a: tuple, b: tuple):  # Snake should avoid zig-zag for A*
+    mn = min(a[0], b[0])
+    mx = max(a[0], b[0])
+    xdist = min((mx - mn), (mn + BLOCKS_H - mx))
+
+    mn = min(a[1], b[1])
+    mx = max(a[1], b[1])
+    ydist = min((mx - mn), (mn + BLOCKS_V - mx))
+    return xdist + ydist
+
+
+def find_path(previous, visited):
     visited.fill(False)
     mark_snake_on_board()
     head = snake[0]
@@ -150,11 +179,13 @@ def find_path():
     previous[head.pos] = (-1, -1)
     heap = [(0, head.pos)]
     closest_pos = head.pos
-    while closest_pos != food and len(heap) != 0:
+    while closest_pos != food:
+        if len(heap) == 0:
+            return Direction.NONE
         closest_dist, closest_pos = heappop(heap)
         for neighbour, direction in neighbours(closest_pos):
             if not visited[neighbour]:
-                heappush(heap, (closest_dist + 1, neighbour))
+                heappush(heap, (points_2d_dist_manhattan(food, neighbour), neighbour))
                 previous[neighbour] = closest_pos
         visited[closest_pos] = True
     node = food
@@ -203,12 +234,15 @@ def main():
     while True:
         input(pygame.event.get())
         global movement
-        movement = find_path()
-        move_snake()
-        check_food()
-        draw_scene(screen)
-        pygame.time.delay(400)
-
+        movement = find_path(previous, visited)
+        if movement == Direction.NONE:
+            print("GAME OVER")
+            sleep(5)
+        else:
+            move_snake()
+            check_food()
+            draw_scene(screen)
+            pygame.time.delay(80)
 
 if __name__ == '__main__':
     main()
